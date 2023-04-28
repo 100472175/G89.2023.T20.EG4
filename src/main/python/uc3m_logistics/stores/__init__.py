07a7efac63802371cf57f5c04c3a1e049539273d
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-04-28 10:40:53 +0200

    Fixed 2 pylint errors, for the sake of understandability  attributes were not taken into account, theres also error of the Singleton implementation not using PascalCase beacuse of the __ and circular imports

--- src/main/python/uc3m_logistics/stores/__init__.py
+++ src/main/python/uc3m_logistics/stores/__init__.py
@@ -1,3 +1,4 @@
+"""Store MODULE WITH ALL THE FEATURES REQUIRED FOR ACCESS CONTROL"""
 from .jsons_store import JsonStore
 from .order_delivery_store import OrderDeliveryStore
 from .order_request_store import OrderRequestStore
